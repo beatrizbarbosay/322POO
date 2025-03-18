45683a0a897e5d705a5baa34096855b7d3f1ff78
Ana Beatriz Barbosa Yoshida <245609@dac.unicamp.br>
2025-03-18 15:28:59 -0300

    muda as classes robos e ambientes para utilizar no lab02

--- lab02/Main.java
+++ lab02/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         // Criando o ambiente
-        Ambiente ambiente = new Ambiente(100, 100);
+        Ambiente ambiente = new Ambiente(100, 100, 100);
 
         // Criando um robô
-        Robo robo = new Robo("Robson", 3, 22);
+        Robo robo = new Robo("Robson", 3, 22, "Norte");
 
         // Movendo o robô
         robo.mover(2,33);
