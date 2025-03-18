51c724ec4e0b45473ae91d49b1061e1ab6bdbebe
FaKarasz <fabricio.karasz@gmail.com>
2025-03-18 14:56:32 -0300

    arruma pasta lab02

---
+++ lab02/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+        // Criando o ambiente
+        Ambiente ambiente = new Ambiente(100, 100);
+
+        // Criando um robô
+        Robo robo = new Robo("Robson", 3, 22);
+
+        // Movendo o robô
+        robo.mover(2,33);
+
+        // Exibindo a posição
+        robo.exibirPosicao();
+
+        // Verificando se está dentro do ambiente
+        if (ambiente.dentroDosLimites(robo.retornarX(),robo.retornarY())) {
+            System.out.println("O robô está dentro dos limites do ambiente.");
+        } else {
+            System.out.println("O robô está fora dos limites do ambiente.");
+        }
+    }
+}
